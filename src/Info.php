a27e003eb2bdce76e976962fdaec5b2ff7cdc2f9
Stanimir <stanimir.k.stoyanov@gmail.com>
2017-06-08 09:37:46 +0300

    Add options page

--- src/Info.php
+++ src/Info.php
@@ -8,6 +8,9 @@ class Info {
 	function __construct() {
 		// Hook our plugins_loaded function
 		add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
+
+		// Add plugin options page to main settings
+		add_action( 'admin_menu', array( $this, 'add_options_page' ) );
 	}
 
 	/**
@@ -17,5 +20,48 @@ class Info {
 	public function load_textdomain() {
 		// initialize translations
 		load_plugin_textdomain( 'php-info', false, PHP_INFO_DIR . '/languages' );
+	}
+
+	/**
+	 * Get the title of the submenu item page.
+	 *
+	 * @access public
+	 *
+	 * @return string $menu_title The title of the submenu item.
+	 */
+	public function get_menu_title() {
+		// allow filtering the title of the submenu page
+		$menu_title = apply_filters('php_info_menu_item_title', __( 'Php Info', 'php-info' ) );
+
+		return $menu_title;
+	}
+
+	/**
+	 * Add plugin settings page.
+	 *
+	 * @access public
+	 */
+	public function add_options_page() {
+		// Get menu title
+		$menu_title = $this->get_menu_title();
+
+		// register the submenu page - child of the Settings parent menu item
+		add_submenu_page(
+			'options-general.php',
+			$menu_title,
+			$menu_title,
+			'publish_posts',
+			'phpinfo',
+			array( $this, 'render' )
+		);
+	}
+
+	/**
+	 * Callabck function for options page.
+	 *
+	 * @access public
+	 */
+	public function render() {
+		include_once( PHP_INFO_DIR . '/templates/options.php' );
 	}
 }
---
+++ templates/options.php
@@ -0,0 +1,7 @@
+<div class="phpinfo-wrapper">
+	<div class="wrap">
+		<h2>
+			<?php echo $this->get_menu_title(); ?>
+		</h2>
+	</div>
+</div>
